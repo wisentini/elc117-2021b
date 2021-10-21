fb380a75e6e389283047f0e15766a1808dfeaed1
AndreaInfUFSM <andrea@inf.ufsm.br>
2021-10-20 21:12:53 -0300

    Add pratica java02

---
+++ praticas/java02/src/Circle.java
@@ -0,0 +1,14 @@
+public class Circle {
+   private double x;
+   private double y;
+   private double r;
+   
+   public Circle(double r) {
+      this.x = this.y = 0.0;
+      this.r = r;
+      System.out.println("New Circle");
+   }
+   public double area() {
+      return Math.PI * r * r;
+   }
+}
