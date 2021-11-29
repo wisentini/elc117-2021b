da422552b3b304a411eb7094b1ab79eb5889fccb
AndreaInfUFSM <andrea@inf.ufsm.br>
2021-11-29 19:22:43 -0300

    Add pratica java07

---
+++ praticas/java07/README.md
@@ -0,0 +1,37 @@
+# Prática: Exceções em Java
+
+
+## Objetivos
+Nesta prática, você vai trabalhar com exceções em Java, que se baseiam fortemente no conceito de herança de POO.
+
+## Pré-requisitos
+
+Antes desta prática, consulte o material sobre exceções em Java (<a href="https://docs.google.com/presentation/d/1qaRbIe9X0UjKwHwnFqHvNK-fS97LCzm4CaXX0NGMhCo/edit?usp=sharing">slides</a>).
+
+
+## Entrega
+
+Esta prática vai ser entregue em um repositório específico no GitHub Classroom. Clique [aqui](https://classroom.github.com/a/xibKSpK3) para criá-lo. 
+
+
+## Preparação: exemplos de exceções
+
+Esta parte não precisa ser entregue!
+
+No arquivo [ExceptionExamples.java](src/ExceptionExamples.java) há vários exemplos envolvendo exceções, tratadas ou não, verificadas ou não (checked/unchecked). São várias pequenas classes no mesmo arquivo, para facilitar a visualização dos exemplos.
+
+1. Compile o código e execute cada uma das classes (note que todas têm seu método main). 
+
+2. Identifique todas as classes de exceções dos exemplos em [ExceptionExamples.java](src/ExceptionExamples.java). Consulte a documentação de referência do Java e descubra a hierarquia de classes de cada uma das classes de exceção dos exemplos.
+
+3. Na classe `CheckedExceptionTryCatch`, temos `catch` para as exceções `FileNotFoundException` e `IOException`. Estas exceções são verificadas (checked) pelo compilador. Se removermos as 2 linhas referentes ao `catch` de `FileNotFoundException`, haverá erro na compilação do programa? Por quê?
+
+
+## Exercício
+
+No arquivo [HttpURLConnectionExample.java](HttpURLConnectionExample.java), há um programa que faz uma requisição pela rede a uma URL. Compile e execute o programa.
+
+Alere um caractere da URL (variável `urlstr`) para torná-la inválida. Compile e execute o programa novamente. Como a URL alterada, o programa produzirá uma exceção e terminará abruptamente. Como o main está declarado com `throws Exception`, a exceção não é capturada e tratada.
+
+Altere o programa para capturar e tratar a exceção que ocorre quando a URL não pode ser acessada.
+
---
+++ praticas/java07/src/ExceptionExamples.java
@@ -0,0 +1,123 @@
+import java.io.*;
+
+class ExceptionExample1 {
+  public static void main(String[] args) {
+    Integer i = null;
+    float f = i.floatValue();
+    System.out.println(f);
+    System.out.println("End");
+  }
+}
+
+class ExceptionExample2 {
+  public static void main(String[] args) {
+    try {
+      Integer i = null;
+      float f = i.floatValue();
+      System.out.println(f);
+    } catch (NullPointerException e) {
+      System.out.print("Caught! ");
+    }
+    System.out.println("End");
+  }
+}
+
+class ExceptionExample3 {
+  public static void main(String[] args) {
+    try {
+      Integer i = null;
+      float f = i.floatValue();
+      System.out.println(f);
+    } catch (NullPointerException e) {
+      System.out.print("Caught! ");
+      return;
+    }
+    System.out.println("End");
+  }
+}
+
+class ExceptionExample4 {
+  public static void main(String[] args) {
+    try {
+      Integer i = null;
+      float f = i.floatValue();
+      System.out.println(f);
+    } catch (NullPointerException e) {
+      System.out.print("Caught! ");
+      return;
+    } finally {
+      System.out.println("Finally");
+    }
+    System.out.println("End");
+  }
+}
+
+class ExceptionExample5 {
+  public static void main(String[] args) {
+    Integer.parseInt("a");
+    System.out.println("End");
+  }
+}
+
+class ExceptionExample6 {
+  public static void main(String[] args) {
+    try {
+      Integer.parseInt("a");
+    } catch (NumberFormatException e) {
+      System.out.println("Invalid number!");
+    }
+    System.out.println("End");
+  }
+}
+
+// class CheckedExceptionError {
+//   public static void main(String args[]) {
+//     FileInputStream is = null;
+//     is = new FileInputStream("notfound.txt");
+//     int i;
+//     while ((i = is.read()) != -1) {
+//       System.out.print((char) i);
+//     }
+//     is.close();
+//   }
+// }
+
+class CheckedExceptionTryCatch {
+  public static void main(String args[]) {
+    FileInputStream is = null;
+    try {
+      is = new FileInputStream("notfound.txt");
+      int i;
+      while ((i = is.read()) != -1) {
+        System.out.print((char) i);
+      }
+    } catch (FileNotFoundException e) {
+      System.out.println("File not found!");
+      //e.printStackTrace();
+    } catch (IOException e) {
+      System.out.println("I/O error: " + e);
+    } finally {
+      System.out.println("Finally!");
+      if (is != null) {
+        try {
+          is.close();
+        } catch (IOException e) {
+          System.out.println("I/O error: " + e);
+        }
+      }
+    }
+  }
+}
+
+class CheckedExceptionThrows {
+  public static void main(String args[])
+      throws IOException {
+    FileInputStream is = null;
+    is = new FileInputStream("notfound.txt");
+    int i;
+    while ((i = is.read()) != -1) {
+      System.out.print((char) i);
+    }
+    is.close();
+  }
+}
